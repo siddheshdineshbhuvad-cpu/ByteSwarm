787174146ee2c1ab39b9125343542b86c9198b69
siddheshdineshbhuvad-cpu <siddheshdineshbhuvad@gmail.com>
2026-08-10 15:55:13 +0530

    Day 1: Setup Spring Boot backend and hello API

---
+++ backend/src/main/java/com/byteSwarm/bytesWarm/BytesWarmApplication.java
@@ -0,0 +1,13 @@
+package com.byteSwarm.bytesWarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BytesWarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BytesWarmApplication.class, args);
+	}
+
+}
